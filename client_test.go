46e033f5002f461d5e197773af3f9e74a8921c32
German Gorelkin <german.gorelkin@gmail.com>
2020-04-09 13:29:26 +0300

    add TestGetPredict_Error

--- client_test.go
+++ client_test.go
@@ -15,11 +15,16 @@ import (
 
 var (
 	responsePredictSuccessful []byte
+	responsePredictError      []byte
 )
 
 func TestMain(m *testing.M) {
 	var err error
 	responsePredictSuccessful, err = ioutil.ReadFile("testdata/resp_predict_successful")
+	if err != nil {
+		log.Fatal(err)
+	}
+	responsePredictError, err = ioutil.ReadFile("testdata/resp_predict_error")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,5 +76,54 @@ func TestGetPredict_Successful(t *testing.T) {
 
 	got, err := cl.GetPredict(context.Background(), products)
 	assert.Nil(t, err)
+	assert.Nil(t, got.Error)
+	assert.Equal(t, want, got)
+}
+
+func TestGetPredict_Error(t *testing.T) {
+	want := new(PredictResponse)
+	err := json.Unmarshal(responsePredictError, want)
+	assert.Nil(t, err)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+
+		_, err = w.Write(responsePredictError)
+		assert.Nil(t, err)
+	}))
+	defer ts.Close()
+
+	// ---
+	cl, err := NewClient(ClientConfig{
+		HTTPClientTimeout: 30 * time.Second,
+		BaseURL:           ts.URL,
+		Token:             "token",
+	})
+	assert.Nil(t, err)
+
+	// ---
+	products := []Product{
+		{
+			ProductId: "9000101411423",
+			Dates: ProductDates{
+				ShipmentDateFrom: "2020-05-26",
+				ShipmentDateTo:   "2020-06-01",
+				ShelfDateFrom:    "2020-05-01",
+				ShelfDateTo:      "2020-05-14",
+			},
+			Parameters: ProductParameters{
+				Client:      "Pyaterochka",
+				Type:        "Mega",
+				Price:       "208.83",
+				DiscountPpd: "0.47000000",
+				DiscountOff: "0.413000",
+				DiscountOn:  "0.08000000",
+			},
+		},
+	}
+
+	got, err := cl.GetPredict(context.Background(), products)
+	assert.Nil(t, err)
+	assert.Nil(t, got.Result)
 	assert.Equal(t, want, got)
 }
